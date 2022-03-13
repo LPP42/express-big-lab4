0ec2d2a67c0cb24926518dcfd416235d23ee03c1
LPP <2175801@cegep-heritage.qc.ca>
2022-03-13 14:12:33 -0400

    half working menu

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
+const async = require('hbs/lib/async');
 var router = express.Router();
 
+const pageModel = require('../model/page');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'LPP\'s example of lab 4' });
+router.get('/', async function(req, res, next) {
+
+  let result = await pageModel.getPages();
+  res.render('pages', { title: 'LPP\'s example of lab 4', pages: result });
+
+
+  // res.render('index', { title: 'LPP\'s example of lab 4' });
 });
 
 module.exports = router;
--- routes/pages.js
+++ routes/pages.js
@@ -6,14 +6,16 @@ const pageModel = require('../model/page');
 /* GET pages listing. */
 router.get('/', async function(req, res) {
     let result = await pageModel.getPages();
-    res.render('pages', { title: "All pages", pages: result });
+    res.render('menu', { title: "All pages", pages: result });
 });
-router.get('/:pageId', async function(req, res) {
 
+//Get specified page
+router.get('/:pageId', async function(req, res) {
     let pageId = parseInt(req.params.pageId.trim());
     let result = await pageModel.getPage(pageId);
     //console.log(result);
-    res.send(result);
+    // res.send(result);
+    res.render('specifiedPage', { title: "Specified page", pages: result });
 });
 
 
